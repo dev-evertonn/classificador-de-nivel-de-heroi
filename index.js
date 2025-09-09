2b7e0d188810a30d5e54131d1c164fe4ca110af3
dev-evertonn <evertonporalves73@gmail.com>
2025-09-09 16:25:43 -0300

    projeto finalizado

---
+++ index.js
@@ -0,0 +1,25 @@
+import entrada from "readline-sync";
+
+let nome = entrada.question("Digite seu nome \n");
+let xp = entrada.question("Digite sua quantidade de  experiência (XP) \n");
+let nivel = "";
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome "${nome}" está no nível de ${nivel}`);
